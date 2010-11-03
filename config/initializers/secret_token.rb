a217b1f2214941783f82203b45f45fc505f058c6
John Graff <482722@gmail.com>
2010-11-03 17:30:58 -0400

    started over... again

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TaskManager::Application.config.secret_token = 'af888d2ae3d14fba2276ec77eaebdb09637c4f00a399f6ee11a5bc21965f4a194389aa88ddf098ff5c84baacdb9a5bbecd344c0b70cb34e20fd87d5b1b30c121'
+TaskManager::Application.config.secret_token = '8a818d86495abe349648a6a70019959dc3bd44f4d5cfc22295b0cddf09cc8d7f9ce0c60ef6d4a23c48d6e802cabef71d8a820c8a29b14a0074032d13920be6f0'
